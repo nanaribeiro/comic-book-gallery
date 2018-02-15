2b3f9a3022ad232e5387fc2f4eaaa0f658e26ae6
nanaribeiro <alanaribeiroramos@gmail.com>
2018-02-14 23:12:41 -0200

    Updated detail method to return ActionResult

--- src/ComicBookGalery/Controllers/ComicBooksController.cs
+++ src/ComicBookGalery/Controllers/ComicBooksController.cs
@@ -8,9 +8,13 @@ namespace ComicBookGalery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic book controller";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+            return Content( "Hello from the comic book controller");
         }
     }
 }
