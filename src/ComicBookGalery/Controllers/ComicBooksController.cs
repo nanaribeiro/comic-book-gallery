375433c05998260d5d528d5f66cd1dec9b83a12d
nanaribeiro <alanaribeiroramos@gmail.com>
2018-02-14 22:58:07 -0200

    Added detail action

--- src/ComicBookGalery/Controllers/ComicBooksController.cs
+++ src/ComicBookGalery/Controllers/ComicBooksController.cs
@@ -8,6 +8,9 @@ namespace ComicBookGalery.Controllers
 {
     public class ComicBooksController : Controller
     {
-
+        public string Detail()
+        {
+            return "Hello from the comic book controller";
+        }
     }
 }
